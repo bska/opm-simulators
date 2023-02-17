7b0a850b6ec0b1f9046dc4b4eaa1a546c4818331
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-02-17 12:01:28 +0100

    x

--- opm/simulators/timestepping/ConvergenceReport.hpp
+++ opm/simulators/timestepping/ConvergenceReport.hpp
@@ -378,6 +378,11 @@ namespace Opm
             return res_convergence_;
         }
 
+        const std::vector<ReservoirConvergenceMetric>& wellConvergence() const
+        {
+            return this->well_convergence_;
+        }
+
         const std::vector<WellFailure>& wellFailures() const
         {
             return well_failures_;
