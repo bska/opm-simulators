fa3ae2185feab353a5d896403dcc77272f33ca52
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-02-17 12:01:28 +0100

    x

--- opm/simulators/flow/BlackoilModel.hpp
+++ opm/simulators/flow/BlackoilModel.hpp
@@ -35,13 +35,13 @@
 #include <opm/simulators/linalg/ISTLSolver.hpp>
 
 #include <opm/simulators/timestepping/ConvergenceReport.hpp>
+#include <opm/simulators/timestepping/NonlinearConvergenceRate.hpp>
 #include <opm/simulators/timestepping/SimulatorReport.hpp>
 #include <opm/simulators/timestepping/SimulatorTimer.hpp>
 
 #include <opm/simulators/utils/BlackoilPhases.hpp>
 #include <opm/simulators/utils/ComponentName.hpp>
 
-#include <opm/simulators/wells/BlackoilWellModel.hpp>
 
 #include <memory>
 #include <tuple>
