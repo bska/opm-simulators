109d1eb76f8742de004b908a820718395933c63c
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-10-13 17:47:58 +0200

    Add Regression Tests for EDITNNC/MULTREGT Combination
    To guard against this combination failing in the future.

--- regressionTests.cmake
+++ regressionTests.cmake
@@ -813,6 +813,30 @@ add_test_compareECLFiles(CASENAME nonnc
                          REL_TOL ${rel_tol}
                          DIR editnnc)
 
+add_test_compareECLFiles(CASENAME editnnc_multregt
+                         FILENAME EDITNNC_AND_MULTREGT
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR editnnc
+                         TEST_ARGS --solver-max-time-step-in-days=0.1)
+
+add_test_compareECLFiles(CASENAME nnc_overlapping_editnnc
+                         FILENAME NNC_OVERLAPPING_EDITNNC
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR editnnc
+                         TEST_ARGS --enable-opm-rst-file=1)
+
+add_test_compareECLFiles(CASENAME nnc_overlapping_editnncr_multregt
+                         FILENAME NNC_OVERLAPPING_EDITNNCR_MULTREGT
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR editnnc
+                         TEST_ARGS --enable-opm-rst-file=1)
+
 add_test_compareECLFiles(CASENAME spe1_foam
                          FILENAME SPE1FOAM
                          SIMULATOR flow
