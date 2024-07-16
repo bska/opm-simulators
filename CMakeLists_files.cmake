4fca80c8f52e4a35fc261ebe337fb23efc6bfd30
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-07-16 18:23:13 +0200

    x

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -139,6 +139,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/flow/partitionCells.cpp
   opm/simulators/flow/RFTContainer.cpp
   opm/simulators/flow/RSTConv.cpp
+  opm/simulators/flow/RegionConcVariation.cpp
   opm/simulators/flow/RegionPhasePVAverage.cpp
   opm/simulators/flow/SimulatorConvergenceOutput.cpp
   opm/simulators/flow/SimulatorFullyImplicitBlackoil.cpp
@@ -949,6 +950,8 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/flow/NewTranFluxModule.hpp
   opm/simulators/flow/NlddReporting.hpp
   opm/simulators/flow/NonlinearSolver.hpp
+  opm/simulators/flow/NormalisedTotalConcVariation.hpp
+  opm/simulators/flow/NormalisedTotalConcVariation_impl.hpp
   opm/simulators/flow/OutputBlackoilModule.hpp
   opm/simulators/flow/OutputCompositionalModule.hpp
   opm/simulators/flow/OutputExtractor.hpp
@@ -957,6 +960,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/flow/priVarsPacking.hpp
   opm/simulators/flow/RFTContainer.hpp
   opm/simulators/flow/RSTConv.hpp
+  opm/simulators/flow/RegionConcVariation.hpp
   opm/simulators/flow/RegionPhasePVAverage.hpp
   opm/simulators/flow/SimulatorConvergenceOutput.hpp
   opm/simulators/flow/SimulatorFullyImplicitBlackoil.hpp
